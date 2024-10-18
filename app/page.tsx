b64d5829d2a0fc090f8748d2c10a1c5569d0ff08
albert19_ <albertnanda1902@gmail.com>
2024-10-18 22:40:09 +0800

    feat: add public assets

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,22 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div>
-      <h1 className="text-white text-3xl underline">Home</h1>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <div className="flex items-center mb-12 gap-2 h-10">
+            <Image
+              src={"/assets/icons/logo.png"}
+              height={1000}
+              width={1000}
+              alt="patient"
+              className="h-full w-fit"
+            />
+            <h1>AlCare</h1>
+          </div>
+        </div>
+      </section>
     </div>
   );
 }
